8252257400346de8576d4d5f17ca241f42fa21b4
selva <selvaraj.antonyraj@sc.com>
2016-10-18 18:01:40 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -2,5 +2,18 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-iframe-resizer'
+  name: 'ember-cli-iframe-resizer',
+  init: function(name) {
+    this._super.init && this._super.init.apply(this, arguments);
+
+    var assets_path = require('path').join('iframe-resizer','index.js');
+    this.treePaths['vendor'] = require.resolve('iframe-resizer').replace(assets_path, '');
+  },
+  included: function(app) {
+    var options = this.app.options.iframeResizerOptions || {enabled: true};
+    if (options.enabled) {
+      this.app.import('vendor/iframe-resizer/js/iframeResizer.js');
+      this.app.import('vendor/iframe-resizer/js/iframeResizer.contentWindow.js');
+    }
+  }
 };
